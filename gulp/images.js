c13023393c114db86dc582f8232d627942afbc45
Matthew Craig <matthew.craig@oneadvertising.ca>
2015-05-26 09:47:08 -0400

    Add back in image cacheing, minor fixes
    - Add back in image cacheing to minifying images
    - CSSNext takes care of the rem fallback for our rem mixin
    - Bug fix for fonts task not copying fonts
    - Dep updates

--- gulp/images.js
+++ gulp/images.js
@@ -9,13 +9,14 @@ module.exports = function (gulp, $, config) {
 	// --------------------------------------|
 	gulp.task('images:minify', function() {
 		return gulp.src(config.images.src)
-			.pipe($.imagemin({
+			.pipe($.if($.if.isFile, $.cache($.imagemin({
 				progressive: true,
 				interlaced: true,
 				svgoPlugins: [{
 					cleanupIDs: false
 				}]
 			}))
+			.on('error', function(err){ console.log(err); this.end; })))
 			.pipe(gulp.dest(config.images.dest));
 	});
 
